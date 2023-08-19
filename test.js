13cb2af1a6746087cb2dbfe4d0f5a15db79f90ab
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-08-19 21:39:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 function check(str, bracketsConfig) {
    
-    
     let brackets = bracketsConfig.join('').replace(/,/g, '');
         let stack = [];
     for (let bracket of str) {
             let bracketsIndex = brackets.indexOf(bracket)
 
             if (bracketsIndex % 2 === 0) {
-                stack.push(bracketsIndex)
-                if (bracket === brackets[bracketsIndex+1]){
-                    stack.pop();
-                }
                 
+                if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+                    stack.pop();
+                } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                    stack.push(bracketsIndex)
+                }
+                else{
+                    stack.push(bracketsIndex)
+                }
             } 
             else {
                 if (stack.pop() !== bracketsIndex-1){
